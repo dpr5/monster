aa226f70d3bef527a8fdbd5d50140187119766e6
Dharmesh Rana <meshie75@gmail.com>
2022-01-09 14:50:30 -0500

    Initial Commit

---
+++ monster/monsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  monsterApp.swift
+//  monster
+//
+//  Created by Drew Seta on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct monsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
