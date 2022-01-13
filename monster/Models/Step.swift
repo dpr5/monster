03f3b7b259321e13067558dc8b8fa1f6ef1d4454
Dharmesh Rana <meshie75@gmail.com>
2022-01-12 22:09:49 -0500

    working base project

---
+++ monster/Models/Step.swift
@@ -0,0 +1,14 @@
+//
+//  Step.swift
+//  monster
+//
+//  Created by Drew Seta on 1/12/22.
+//
+
+import Foundation
+
+struct Step: Identifiable {
+    let id = UUID()
+    let count: Int
+    let date: Date
+}
